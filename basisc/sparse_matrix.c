c14782073ad2adbd0f40d678687619c4e231ac52
sujan05-jpg <sujanbn06@gmail.com>
2026-04-29 11:36:29 +0530

    git add sparse_matrix.c

---
+++ basisc/sparse_matrix.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int matrix[10][10], sparse[20][3];
+    int rows, cols, i, j, k = 0;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    printf("Enter matrix elements:\n");
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            if(matrix[i][j] != 0) {
+                sparse[k][0] = i;
+                sparse[k][1] = j;
+                sparse[k][2] = matrix[i][j];
+                k++;
+            }
+        }
+    }
+
+    printf("\nSparse Matrix (Triplet form):\n");
+    printf("Row Col Value\n");
+    for(i = 0; i < k; i++) {
+        printf("%d %d %d\n", sparse[i][0], sparse[i][1], sparse[i][2]);
+    }
+
+    return 0;
+}
